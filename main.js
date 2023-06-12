34fd3262bcb000dc1862bfb30b2a860e6333271e
MKas <ma2282@yandex.ru>
2023-06-12 03:35:18 +0300

    Init commit

---
+++ main.js
@@ -0,0 +1,111 @@
+let digitButtons = document.querySelectorAll(".digit");
+let label = document.querySelector(".label");
+
+let clearButton = document.querySelector(".clear");
+let powButton = document.querySelector(".pow");
+let divButton = document.querySelector(".div");
+let multButton = document.querySelector(".mult");
+let addButton = document.querySelector(".add");
+let subButton = document.querySelector(".sub");
+let equalButton = document.querySelector(".equal");
+
+let result = 0;
+let lastDigit = 0;
+let lastOperation = "null";
+let lastTextContent = "";
+let isLastDigit = true;
+
+for (let i = 0; i < digitButtons.length; i++)
+{
+    let digit = (i + 1) % 10;
+    digitButtons[i].onclick = function() {
+        if (lastOperation == "null" && lastTextContent == "") {
+            result = label.textContent = lastDigit =
+            lastTextContent = digit;
+            isLastDigit = true;
+        }
+        else if (isLastDigit)
+        {
+            lastDigit = lastDigit * 10 + digit;
+            label.textContent += digit;
+        }
+        else {
+            lastDigit = digit;
+            label.textContent += digit;
+            isLastDigit = true;
+        }
+        
+        if (!isLastDigit)
+            processResult();
+    }
+}
+
+function processResult() {
+    switch (lastOperation) {
+        case "^":
+            result **= lastDigit;
+            break;
+        case "/":
+            result /= lastDigit;
+            break;
+        case "*":
+            result *= lastDigit;
+            break;
+        case "+":
+            result += lastDigit;
+            break;
+        case "-":
+            result -= lastDigit;
+            break;
+    }
+    lastOperation = "null";
+    isLastDigit = false;
+}
+
+function showResult() {
+    processResult();
+    label.textContent = result;
+}
+
+function processButton(operation) {
+    if (!isLastDigit)
+        label.textContent = lastTextContent;
+    else
+        processResult();
+
+    lastOperation = operation;
+    lastTextContent = label.textContent;
+    label.textContent += " " + operation + " ";
+}
+
+clearButton.onclick = function() {
+    result = 0;
+    label.textContent = result;
+    lastOperation = "null";
+    lastTextContent = "";
+}
+
+powButton.onclick = function() {
+    processButton("^");
+}
+
+divButton.onclick = function() {
+    processButton("/");
+}
+
+multButton.onclick = function() {
+    processButton("*");
+}
+
+addButton.onclick = function() {
+    processButton("+");
+}
+
+subButton.onclick = function() {
+    processButton("-");
+}
+
+equalButton.onclick = function() {
+    showResult();
+    lastOperation = "null";
+}
